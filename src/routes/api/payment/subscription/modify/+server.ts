1e5f4da82e0232dc54d62f7a50606828b6d889ed
AmirSolt <soltamir194@gmail.com>
2023-05-15 15:24:30 -0400

    added update subscription

---
+++ src/routes/api/payment/subscription/modify/+server.ts
@@ -0,0 +1,30 @@
+
+import { stripe } from '$lib/server/stripe'
+
+
+export const POST = async () => {
+    
+    return new Response();
+};
+
+
+
+async function updateSubscription(request, response){
+    const subscription = await stripe.subscriptions.retrieve(
+        request.body.subscriptionId
+      );
+      const updatedSubscription = await stripe.subscriptions.update(
+        request.body.subscriptionId,
+        {
+          cancel_at_period_end: false,
+          items: [
+            {
+              id: subscription.items.data[0].id,
+              price: "price_H1NlVtpo6ubk0m",
+            },
+          ],
+        }
+      );
+    
+      response.send(updatedSubscription);
+}
